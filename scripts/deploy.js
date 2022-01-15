1bb295b626d0e15d0f25d740b8b933957c406d89
Denis Granha <denisgranha@gmail.com>
2022-01-15 23:12:07 +0100

    initial Rumia NFT ERC 721

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const RumiaNFT = await hre.ethers.getContractFactory("RumiaNFT");
+  const rumiaNFT = await RumiaNFT.deploy();
+
+  await rumiaNFT.deployed();
+
+  // This solves the bug in Mumbai network where the contract address is not the real one
+  const txHash = rumiaNFT.deployTransaction.hash;
+  console.log(`Tx hash: ${txHash}\nWaiting for transaction to be mined...`);
+  const txReceipt = await ethers.provider.waitForTransaction(txHash);
+
+  console.log("RUMIA NFT deployed to:", txReceipt.contractAddress);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
